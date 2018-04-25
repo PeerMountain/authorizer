7666d095f47fbb4e8dab3503ea632dc86ca066e2
Jonatas Baldin <jonatas.baldin@gmail.com>
2018-04-25 21:09:28 +0530

    Add checks for BROKER_URL, READER_HOSTNAME, READER_HOSTNAME env vars

--- authorizer/settings.py
+++ authorizer/settings.py
@@ -1,5 +1,21 @@
+from os import getenv
 import logging
 import logging.config
+
+
+BROKER_URL = getenv('BROKER_URL')
+if not BROKER_URL:
+    raise NotImplementedError('BROKER_URL must be configured.')
+
+
+READER_HOSTNAME = getenv('READER_HOSTNAME')
+if not READER_HOSTNAME:
+    raise NotImplementedError('READER_HOSTNAME must be configured.')
+
+
+READER_PORT = getenv('READER_PORT')
+if not READER_PORT:
+    raise NotImplementedError('READER_PORT must be configured.')
 
 
 def configure_logging():
